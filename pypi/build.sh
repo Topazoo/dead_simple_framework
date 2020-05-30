1ef7855a3634236478b279ef281b085da38dad18
Peter Swanson <topazoo42@gmail.com>
2020-05-30 04:32:17 -0700

    another packaging fix

--- pypi/build.sh
+++ pypi/build.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-rm -rf dist/ build/ dead_simple_framework.egg-info/
+rm -rf dist/ build/ dead_simple_framework.egg-info/ dead_simple_framework/ README.md
 cp -r ../README.md ../dead_simple_framework .
 python3 setup.py sdist bdist_wheel
-rm -rf dead_simple_framework/
