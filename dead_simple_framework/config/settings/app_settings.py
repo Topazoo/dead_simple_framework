7c823731fb287aecac28259c05bc2d60f1cefd7c
Peter Swanson <topazoo42@gmail.com>
2020-12-18 02:54:18 -0800

    Add APP_PORT env var

--- dead_simple_framework/config/settings/app_settings.py
+++ dead_simple_framework/config/settings/app_settings.py
@@ -13,16 +13,18 @@ class App_Settings(Setting):
     APP_ENV = os.environ.get('APP_ENV', 'development')
     APP_ENABLE_CORS = os.environ.get('APP_ENABLE_CORS', False)
     APP_HOST = os.environ.get('APP_HOST', '0.0.0.0')
+    APP_PORT = int(os.environ.get('APP_PORT', '5000'))
     APP_API_CLIENT_HEADERS = {"User-Agent": "Mozilla/5.0"}
     APP_LOG_CONFIG = os.environ.get('APP_LOG_CONFIG', True)
     APP_DEBUG_MODE = True
 
-    def __init__(self, app_env:str=None, app_enable_cors:bool=None, app_host:str=None, app_api_client_headers:dict=None, app_log_config:bool=None):
+    def __init__(self, app_env:str=None, app_enable_cors:bool=None, app_host:str=None, app_port:int=None, app_api_client_headers:dict=None, app_log_config:bool=None):
         if app_env: App_Settings.APP_ENV = app_env
         os.environ['FLASK_ENV'] = App_Settings.APP_ENV
 
         if app_enable_cors != None: App_Settings.APP_ENABLE_CORS = app_enable_cors
         if app_host: App_Settings.APP_HOST = app_host
+        if app_port: App_Settings.APP_PORT = app_port
         if app_api_client_headers: App_Settings.APP_API_CLIENT_HEADERS = app_api_client_headers
         if app_log_config: App_Settings.APP_LOG_CONFIG = app_log_config
         
