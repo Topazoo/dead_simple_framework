0e71ff7b4284da0858425e161c093d45153af753
Peter Swanson <topazoo42@gmail.com>
2021-01-11 21:56:43 -0800

    Build in JWT

--- dead_simple_framework/config/settings/app_settings.py
+++ dead_simple_framework/config/settings/app_settings.py
@@ -16,9 +16,10 @@ class App_Settings(Setting):
     APP_PORT = int(os.environ.get('APP_PORT', '5000'))
     APP_API_CLIENT_HEADERS = {"User-Agent": "Mozilla/5.0"}
     APP_LOG_CONFIG = os.environ.get('APP_LOG_CONFIG', True)
+    APP_JWT_KEY = os.environ.get('APP_JWT_KEY', 'a-secure-key')
     APP_DEBUG_MODE = True
 
-    def __init__(self, app_env:str=None, app_enable_cors:bool=None, app_host:str=None, app_port:int=None, app_api_client_headers:dict=None, app_log_config:bool=None):
+    def __init__(self, app_env:str=None, app_enable_cors:bool=None, app_host:str=None, app_port:int=None, app_api_client_headers:dict=None, app_log_config:bool=None, app_jwt_key:str=None):
         if app_env: App_Settings.APP_ENV = app_env
         os.environ['FLASK_ENV'] = App_Settings.APP_ENV
 
@@ -27,6 +28,8 @@ class App_Settings(Setting):
         if app_port: App_Settings.APP_PORT = app_port
         if app_api_client_headers: App_Settings.APP_API_CLIENT_HEADERS = app_api_client_headers
         if app_log_config: App_Settings.APP_LOG_CONFIG = app_log_config
+
+        if app_jwt_key: App_Settings.APP_JWT_KEY = app_jwt_key
         
         App_Settings.APP_DEBUG_MODE = True if App_Settings.APP_ENV in ['dev', 'development', 'uat'] else False
         os.environ['FLASK_DEBUG'] = '1' if App_Settings.APP_DEBUG_MODE else '0'
