e19ce931d296306908c34666c06f613794cf6bd4
Peter Swanson <topazoo42@gmail.com>
2021-01-22 00:53:59 -0800

    Add explicit jsonschema dependency

--- pypi/setup.py
+++ pypi/setup.py
@@ -12,12 +12,12 @@ setuptools.setup(
     name='dead_simple_framework',
     license="MIT",
     description='RESTful Flask framework with MongoDB, Redis and Celery integrations',
-    version='v0.7.9',
+    version='v0.8.0',
     long_description=README,
     url='https://github.com/Topazoo/dead_simple_framework',
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
-    install_requires=['flask', 'pymongo', 'celery', 'flask-cors', 'requests', 'redis', 'eventlet', 'pyOpenSSL', 'Flask-JWT-Extended', 'passlib'],
+    install_requires=['flask', 'pymongo', 'celery', 'flask-cors', 'requests', 'redis', 'eventlet', 'pyOpenSSL', 'Flask-JWT-Extended', 'passlib', 'jsonschema'],
     long_description_content_type='text/markdown',
     classifiers=[]
 )
