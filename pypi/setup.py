24880edd1ae8da8f4a0983a3b22c1abf03728df0
Peter Swanson <topazoo42@gmail.com>
2022-01-18 22:11:59 -0800

    [PyPi] Bump version

--- pypi/setup.py
+++ pypi/setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     name='dead_simple_framework',
     license="MIT",
     description='RESTful Flask framework with builtin MongoDB, Redis, Celery, Sentry and Slack integrations',
-    version='v1.2.7',
+    version='v1.2.8',
     long_description=README,
     url='https://github.com/Topazoo/dead_simple_framework',
     packages=setuptools.find_packages(),
